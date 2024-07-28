b8714456eb4e5614776d6c4a635d99dd207135b7
Fahad Zulfiqar <malikero2056@gmail.com>
2024-07-28 18:07:44 +0500

    GameUnite frontend

---
+++ src/Pages/Admin/UserListScreen/index.jsx
@@ -0,0 +1,104 @@
+import { LinkContainer } from "react-router-bootstrap";
+import { Table, Button, Container } from "react-bootstrap";
+import { FaTimes, FaEdit, FaTrash, FaCheck } from "react-icons/fa";
+import Message from "../../../components/Message";
+import Loader from "../../../components/Loader";
+import {
+  useGetUsersQuery,
+  useDeleteUsersMutation,
+} from "../../../slices/usersApiSlice";
+import { toast } from "react-toastify";
+
+const UserListScreen = () => {
+  const { data: users, refetch, isLoading, error } = useGetUsersQuery();
+
+  const [deleteUsers, { isLoading: loadingDelete }] = useDeleteUsersMutation();
+
+  const deleteHandler = async (id) => {
+    if (window.confirm("Are you sure?")) {
+      try {
+        await deleteUsers(id);
+        toast.success("User deleted");
+        refetch();
+      } catch (err) {
+        toast.error(err?.data?.message || err.error);
+      }
+    }
+
+    // console.log("deleted");
+  };
+
+  return (
+    <>
+      <Container>
+        <h1>Users</h1>
+        {loadingDelete && <Loader />}
+
+        {isLoading ? (
+          <Loader />
+        ) : error ? (
+          <Message variant="danger">
+            {error?.data?.message || error.error}
+          </Message>
+        ) : (
+          <Table striped hover responsive className="table-sm">
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>NAME</th>
+                <th>EMAIL</th>
+                <th>ADMIN</th>
+                <th>Seller</th>
+
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              {users.map((user) => (
+                <tr key={user._id}>
+                  <td>{user._id}</td>
+                  <td>{user.name}</td>
+                  <td>
+                    <a href={`mailto:${user.email}`} style={{ color: "gray" }}>
+                      {user.email}
+                    </a>
+                  </td>
+                  <td>
+                    {user.isAdmin ? (
+                      <FaCheck style={{ color: "green" }} />
+                    ) : (
+                      <FaTimes style={{ color: "red" }} />
+                    )}
+                  </td>
+                  <td>
+                    {user.isSeller ? (
+                      <FaCheck style={{ color: "green" }} />
+                    ) : (
+                      <FaTimes style={{ color: "red" }} />
+                    )}
+                  </td>
+                  <td>
+                    <LinkContainer to={`admin/user/${user._id}/edit`}>
+                      <Button variant="light" className="btn-sm">
+                        <FaEdit />
+                      </Button>
+                    </LinkContainer>
+                    <Button
+                      variant="danger"
+                      className="btn-sm"
+                      onClick={() => deleteHandler(user._id)}
+                    >
+                      <FaTrash style={{ color: "#fff" }} />
+                    </Button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        )}
+      </Container>
+    </>
+  );
+};
+
+export default UserListScreen;
